9fc7a43707e211c927ec9b91adf9e28ae26c473c
codefug <robot9917@naver.com>
2024-01-01 01:45:50 +0900

    complete customimageslider

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Move from "./move/move";
   arrowLeft.addEventListener("click", () => newNode.backward());
   arrowRight.addEventListener("click", () => newNode.forward());
   Array.from(slideButtons).forEach((element, index) => {
-    element.addEventListener("click", ()=>newNode.exactNumber(index));
+    element.addEventListener("click", () => newNode.exactNumber(index));
   });
+  setInterval(()=>{newNode.forward()},5000);
 })();
